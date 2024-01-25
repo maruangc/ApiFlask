6ad2e64aafdfe61d9a81e37f8f226bddba43efe2
maruanGC <maruands@hotmail.com>
2024-01-24 23:02:41 -0400

    clase del 24012024 implementacion de JWT

--- src/app.py
+++ src/app.py
@@ -10,6 +10,8 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Post
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token,jwt_required, get_jwt_identity
 #from models import Person
 
 app = Flask(__name__)
@@ -22,7 +24,12 @@ else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY') #Viene de el archivo .env
 MIGRATE = Migrate(app, db)
+
+#Agregando JWT
+jwt = JWTManager(app) #envolver toda la app dentro de jwt
 db.init_app(app)
 CORS(app)
 setup_admin(app)
@@ -55,22 +62,47 @@ def create_user():
     if email is None or password is None:
         return jsonify({'Error': 'email and password required'}),400
 
-    new_user=User(email=email, password=password, is_active=True)
+    hashed_password = generate_password_hash(password)
+    new_user=User(email=email, password=hashed_password, is_active=True)
 
     db.session.add(new_user)
     try:
         db.session.commit()
         return 'User created'
     except Exception as error:
-        db.session.rollback()
+        db.session.rollback(),500
         return 'an error ocurred'
+    
+@app.route("/login", methods=["POST"])
+def user_login():
+    body=request.json
+    email=body.get("email", None)
+    password=body.get("password", None)
+
+    if email is None or password is None:
+        return jsonify({"error": "email and password is required"}),400
+    
+    user = User.query.filter_by(email=email).one_or_none()
+    if user is None:
+        return jsonify({"error": "user not found"}),404
+    password_match = check_password_hash(user.password, password)
+    if not password_match:
+        return jsonify({"error": "password incorrect"}),401
+
+    user_token = create_access_token({'id': user.id, 'email': user.email})
+    return jsonify({"token": user_token})
+
+
+
+
 
 @app.route('/post/<int:user_id>', methods=['POST'])
+@jwt_required()
 def create_post(user_id):
     body=request.json
     description=body.get('description', None)
     src=body.get('src', None)
-
+    return 'probando'
     if description is None:
         return jsonify({'Error': 'description is required'}),400
     
@@ -86,6 +118,7 @@ def create_post(user_id):
     except Exception as error:
         db.session.rollback()
         return 'an error ocurred'
+    
 
 @app.route('/get/<int:user_id>', methods=['GET'])
 def get_user(user_id):
@@ -95,6 +128,7 @@ def get_user(user_id):
     print (user.posts)
     user_post=[post.serialize() for post in user.posts]
     return jsonify({'user': user.serialize(), 'posts': user_post})
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
